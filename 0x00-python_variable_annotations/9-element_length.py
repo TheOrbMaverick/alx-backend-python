25a397bbc3e21bf31f41f872512916f6ba7f51e7
TheOrbMaverick <enangita@gmail.com>
2024-07-04 16:02:46 +0100

    all mandatory tasks

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,10 +3,10 @@
 This module provides a function to return a list of tuples with string lengths.
 """
 
-from typing import List, Tuple, Sequence
+from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: List[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Returns a list of tuples where each tuple contains a string and its length.
     """
